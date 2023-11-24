7d5fb2039766d356ee858e3408d6ec7c70caf490
jisoo03010 <040310jisoo@naver.com>
2023-11-24 17:04:24 +0900

    BaekJoon Level_1 study contents update upload.

---
+++ level_1/step_1.java
@@ -0,0 +1,6 @@
+package level_1;
+public class step_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ level_1/step_5.java
@@ -0,0 +1,13 @@
+package level_1;
+
+    import java.util.Scanner;
+public class step_5 {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+	    Double a;
+        Double b;
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		System.out.println(a / b);
+	}
+}
